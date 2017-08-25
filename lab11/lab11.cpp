cb877209285712058bfc88b46f9579c877e773af
Nick L S <nleeseely001@student.butte.edu>
2017-08-25 01:45:05 +0000

    Lab1.1 submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -12,6 +12,7 @@
     User Input Number
     High Boundary Number
     Low Boundary Number
+    User Tries Tally Number
     
 Then take into account all possible user inputs
     1.  User inputs the correct number
@@ -24,5 +25,6 @@ Then decide what happens for each instance
     3.  User's number is compared to the random number and then:
         a.  If the input number is Higher than the random number output "Go lower!" and subtract 1 from the 10 points tally
         b.  If the input number is Lower than the random number output "Go higher!" and subtract 1 from the 10 points tally 
+    4.  Also need to take into account when user's Tally Number reaches 0, by outputting "Game over" and then restart the loop
         
 */
