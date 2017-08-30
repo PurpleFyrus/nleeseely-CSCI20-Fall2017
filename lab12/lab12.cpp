d757a9f2b3985e69b27e652cd061a9a5974014a0
Nick L S <nleeseely001@student.butte.edu>
2017-08-30 01:37:52 +0000

    Lab1.2 Commit

---
+++ lab12/lab12.cpp
@@ -0,0 +1,45 @@
+//Nick LS
+//8/29/17
+
+//following text art is for a lab, cuz yeah!
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ 
+cout << "                    SSSS    SSSSSSSSSSSSSSSSSSSSSSS                 " << endl;  
+cout << "                   SSSSS    SSSSSSSSSSSSSSSSSSSSSSSS                " << endl;  
+cout << "                  SSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSS               " << endl;  
+cout << "                 SSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSS              " << endl;  
+cout << "                SSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSS             " << endl;      
+cout << "               SSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSS            " << endl;    
+cout << "              SSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSS           " << endl;    
+cout << "             SSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSS          " << endl;    
+cout << "            SSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS         " << endl;    
+cout << "           SSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS        " << endl;    
+cout << "          SSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS       " << endl;    
+cout << "         SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      " << endl;
+cout << "        SSSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS     " << endl;
+cout << "       SSSSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS    " << endl;
+cout << "      SSSSSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS   " << endl;
+cout << "                                                                    " << endl;
+cout << "                                                                    " << endl;
+cout << "      SSSSSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS   " << endl;
+cout << "       SSSSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS    " << endl;
+cout << "        SSSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS     " << endl;
+cout << "         SSSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS      " << endl;
+cout << "          SSSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS       " << endl;
+cout << "           SSSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS        " << endl;
+cout << "            SSSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS         " << endl;
+cout << "             SSSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSSS          " << endl;
+cout << "              SSSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSSS           " << endl;
+cout << "               SSSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSSS            " << endl;
+cout << "                SSSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSSS             " << endl;
+cout << "                 SSSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSSS              " << endl;
+cout << "                  SSSSSS    SSSSSSSSSSSSSSSSSSSSSSSSS               " << endl;
+cout << "                   SSSSS    SSSSSSSSSSSSSSSSSSSSSSSS                " << endl;
+
+return 0;
+}
