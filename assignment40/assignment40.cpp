5553af7d6311e3227e43f9f0af5363f0766666a3
Nick L S <nleeseely001@student.butte.edu>
2017-11-20 02:59:32 +0000

    Assignment 4.0

---
+++ assignment40/assignment40.cpp
@@ -0,0 +1,146 @@
+//Created by: Nick LS
+//Created on: 11/19/17
+
+/*concordance*/
+
+#include <iostream>
+#include <fstream>
+#include <string> 
+#include <cstring> 
+#include <cctype>  
+#include <cstdlib> 
+#include <iomanip>
+using namespace std;
+
+
+int main(){
+    
+    ifstream fin;
+    ofstream fout;
+    
+    string source[100];     //String array used for the first storing of the input .txt file's contents
+    string word[100];       //String array that will hold all the stored words from the source file after being curated
+    string stop[571];       //String array that will hold all the stop words from the stop.txt file
+    int num_occur[100];     //Integer array that will be parallel to the word[100] array, and will have the corresponding occurrences of each word
+    int first_occur[100];   //Integer array that will be parallel to the word[100] array, and will have the corresponding first occurrence of each word
+    string is_new = "yes";  //String variable used later on to differentiate when a word is new or has already been stored before in word[]
+    string is_stop = "no";  //String variable used to tell whether it's a stop word or not
+    string input_text_full;     //String variable used later on to be used to find the First Occurrence of a word from the original input.txt file
+    int num_source_words = 0;   //Integer variable used in a later loop, mainly for the counting of words coming in from the input.txt file, but also used for later loop iteration counters.
+    int num_word_words = 0;     //Integer variable used in a later loop, mainly for the counting of words stored in the string array word[], but also used for later loop iteration counters.
+    int num_stop_words = 0;     //Integer variable used in a later loop, to count the number of words in the stop.txt
+    
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//First is the opening of the input.txt and taking in each word as it's own separate string, and storing it all into source[] array
+    fin.open("input.txt");
+    
+    for(int i=0;!fin.eof(); i++){   //Here we transfer the input.txt file into a string array for safe keeping
+        fin >> source[i];
+        num_source_words = num_source_words + 1;
+    }
+    
+    fin.close();
+    
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Next is the opening of the stop.txt and taking in each word into the stop[] array
+    fin.open("stop.txt");
+    
+    for(int i=0;!fin.eof(); i++){   //here we transfer the contents of stop.txt file into a string array for later use.
+        fin >> stop[i];
+        num_stop_words = num_stop_words + 1;
+    }
+    
+    fin.close();
+    
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------- 
+//Then we create a separate string for later use in regards to first occurrence of a string out of the input.txt
+    for(int i = 0; i < num_source_words; i++){
+        input_text_full = input_text_full + ( " " + source[i]);
+    }
+    
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Then we clean up all the values stored in source[] array, by removing all punctuation
+    for(int i = 0; i < 100; i++){       //This for loop goes through the source string array and removes all punctuation
+        string temp_string = source[i];
+        for(int j = 0; j < temp_string.length(); j++){
+            if(ispunct(temp_string[j])){
+                temp_string.erase(j);
+            }
+        }
+        source[i] = temp_string;
+    }
+//And then we do the same for uppercase, changing it all to lower case  
+    for(int i = 0; i < 100; i++){       //This for loop goes through the source string array and changes all upper case letters to lower case
+        string temp_string = source[i];
+        for(int j = 0; j < temp_string.length(); j++){
+            if(isupper(temp_string[j])){
+                temp_string[j] = tolower(temp_string[j]);
+            }
+        }
+        source[i] = temp_string;
+    }
+    
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//The following section is what goes through the string array source[], stores a new word in the string array word[], and adds a count to the word occurrence if it has already been stored in word[]
+    for (int j = 0; j < num_source_words; j++){
+
+        is_new = "yes";                             //This is set to yes, so that after the main loop's iteration's word being compared (source[j]) is compared to all words already stored in word[], if it did not match at all, it will be stored into word[] as a new found word.
+        is_stop = "no";                             //This is set to no, so that after the following nested loop iterates all the way through, if source[j] did not come out to be a stop word, it will continue onwards in the following if statement
+        
+        for (int h = 0; h < num_stop_words; h++){   //This loop iterates through all the stop words provided by the stop.txt
+            if(source[j] == stop[h]){               //If the main overall loop's iteration's word from source[j] is a stop word
+                is_stop = "yes";                    //Then the string variable is_stop is assigned "yes" so that the following if statement & loops within it are not run.
+            }
+        }
+        
+        if(is_stop == "no"){
+            for (int i = 0; i < num_word_words; i++){              //This loop iterates all the way through word[] so that it can be fully searched through for matching words, in case source[j] has already been found and stored.
+    
+                if(source[j] == word[i]){               //If the main loop's iteration's word from the string array source[] has already been stored in the string array word[]
+                    num_occur[i] = num_occur[i] + 1;    //Then that corresponding word gets +1 to it's occurrence count
+                    is_new = "no";                      //And the is_new counter stays set to no, since it's not a word found.
+                }
+            }
+        
+            if(is_new == "yes"){                        //If the nested loop from a few lines before iterated all the way through, and didn't find any matches of already stored words in word[] to source[j]
+               word[num_word_words] = source[j];        //Then that word is then stored into the word[] array
+               num_word_words = num_word_words + 1;     //And the word count is added 1 so that the next new word entered on a following iteration of the overall loop, won't overwrite this value stored in word[].
+               num_occur[j] = 1;                        //Seeing as word[j] is a new word, then it needs to be given a counter of 1.
+            }
+        }
+    }
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//possible code for first occurrence finder: first_occur[i] = source.find(word[i]);
+
+for(int i=0; i<num_word_words; i++){
+    first_occur[i] = input_text_full.find(word[i]);
+}
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Here is the output of the words, number of occurrences, and location of first occurrences to an html file.
+
+fout.open("output.html");
+
+    for(int i=0; i<num_word_words; i++){
+        fout << word[i] << " " << num_occur[i] << " " << first_occur[i] << endl;
+    }
+
+fout.close();
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Here is the input.txt printed for easier readability
+    for(int i=0; i<num_source_words; i++){
+       cout << source[i] << " ";
+    }
+    
+    cout << endl;       //These are for spacing reasons
+    cout << endl;
+    
+//Here is the printing of the words that got stored in word[] array that weren't stop words, their number of occurrences, and the location of their first occurrence
+    for (int i = 0; i < num_word_words; i++){
+        cout << word[i] << " " << num_occur[i] << " " << first_occur[i] << endl;
+    }
+    
+    return 0;
+}
